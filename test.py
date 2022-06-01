cf6c06bb0dd129b692e7d52e55e545d7bf3059a4
MBC <godmin18@gmail.com>
2022-06-02 00:30:44 +0900

    add table_create

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
-from module import db
+from module.db import db_manger
+from module.config import *
+import pymysql
 
-a = db.db_manger
+# db_con = pymysql.connect(
+#                 host=DB_HOST,
+#                 user=DB_USER,
+#                 passwd=DB_PASSWORD,
+#                 db=DB_NAME,
+#                 charset='utf-8'
+#             )
+a = db_manger().table_create()
