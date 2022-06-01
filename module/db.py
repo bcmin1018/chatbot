3515f31feca44e512b4a72f6e34f4270c5446e78
MBC <godmin18@gmail.com>
2022-06-02 00:24:42 +0900

    add table_create

--- module/db.py
+++ module/db.py
@@ -1,5 +1,5 @@
 import pymysql
-from config import *
+from module.config import *
 
 class db_manger:
     def __init__(self):
@@ -11,28 +11,31 @@ class db_manger:
     #         db     = DB_NAME,
     #         charset= 'utf-8'
     #     )
-    def db_connect():
+    def db_connect(self):
         try:
             db_con = pymysql.connect(
                 host   = DB_HOST,
                 user   = DB_USER,
                 passwd = DB_PASSWORD,
                 db     = DB_NAME,
-                charset= 'utf-8'
+                charset= 'utf8'
             )
         except Exception as e:
             print(e)
 
-    def table_create(self):
+        return db_con
 
+    def table_create(self):
         sql = '''
-            CREATE TABLE IF NOT EXISTS 'chatbot_train_data' (
-            'id' INT UNSIGNED NOT NULL AUTO_INCREMENT,
-            'intent' VARCHAR(45) NULL,
-            'ner' VARCHAR(1024) NULL,
-            'query' TEXT NULL,
-            'answer' TEXT NOT NULL,
-            'answer_image' VARCHAR(2048) NULL,
-            PRIMARY KEY ('id'))
+            CREATE TABLE IF NOT EXISTS `chatbot_train_data` (
+            `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+            `intent` VARCHAR(45) NULL,
+            `ner` VARCHAR(1024) NULL,
+            `query` TEXT NULL,
+            `answer` TEXT NOT NULL,
+            `answer_image` VARCHAR(2048) NULL,
+            PRIMARY KEY (`id`))
         ENGINE = InnoDB DEFAULT CHARSET=utf8
         '''
+        with self.db_con.cursor() as cursor:
+            cursor.execute(sql)
