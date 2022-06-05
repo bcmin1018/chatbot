d51f1c45abf2ee694aa05ff11b4f3a5a9c6980f4
bcmin1018 <bcmin1018@gmail.com>
2022-06-05 14:01:10 +0900

    add Preprocess

---
+++ utils/Preprocess.py
@@ -0,0 +1,23 @@
+from konlpy.tag import Komoran
+
+class Preprocess:
+    def __init__(self, userdic=None):
+        self.komoran = Komoran(userdic=userdic)
+        # 관계언, 기호, 어미, 접미사 제거
+        self.exclusion_tags = [
+            'JKS', 'JKC', 'JKG', 'JKO', 'JKB', 'JKV', 'JKQ',
+            'JX', 'JC',
+            'SF', 'SP', 'SS', 'SE', 'SO',
+            'EP', 'EF', 'EC', 'ETN', 'ETM',
+            'XSN', 'XSV', 'XSA'
+        ]
+    def pos(self, sentence):
+        return self.komoran.pos(sentence)
+
+    def get_keywords(self, pos, without_tag=False):
+        f = lambda x: x in self.exclusion_tags
+        word_list = []
+        for p in pos:
+            if f(p[1]) is False:
+                word_list.append(p if without_tag is False else p[0])
+        return word_list
