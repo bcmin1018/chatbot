23c4c4de56804fc8017393e3241012dc645b20a6
bcmin1018 <bcmin1018@gmail.com>
2022-06-07 18:14:11 +0900

    의도분류노트북업로드

--- model/IntentModel.py
+++ model/IntentModel.py
@@ -2,6 +2,8 @@
 import torch
 from torch import nn
 from torch.nn import functional as F
+from utils.Preprocess import BERTDataset
+from kobert_tokenizer import KoBERTTokenizer
 
 class BERTClassifier(nn.Module):
     def __init__(self, bert, hidden_size=768, num_classes=5, dr_rate=None, params=None):
@@ -34,3 +36,21 @@ class IntentModel:
         self.model_load = self.model.load_state_dict(torch.load(self.model_path))
 
     def predict_class(self, query):
+        self.query = BERTDataset(text=query, bert_tokenizer=tok, max_len=MAX_LEN, pad=True,
+                                    pair=False)
+        self.model_load.eval()
+        correct = 0
+        with torch.no_grad():
+            for data in test_loader:
+                data, target = data.to(device), target.to(device)
+                output = model(data)
+                loss = criterion(output, target)
+                test_loss += loss.item()
+                pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+                correct += pred.eq(target.view_as(pred)).sum().item()
+
+        test_loss /= len(test_loader.dataset)
+
+        print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+            test_loss, correct, len(test_loader.dataset),
+            100. * correct / len(test_loader.dataset)))
--- utils/Preprocess.py
+++ utils/Preprocess.py
@@ -1,4 +1,7 @@
 from konlpy.tag import Komoran
+from kobert_transformers import get_tokenizer, get_kobert_model
+tokenizer = get_tokenizer()
+tokenizer()
 
 class Preprocess:
     def __init__(self, userdic=None):
@@ -21,3 +24,17 @@ class Preprocess:
             if f(p[1]) is False:
                 word_list.append(p if without_tag is False else p[0])
         return word_list
+
+class BERTDataset:
+    def __init__(self, text, bert_tokenizer, max_len, pad, pair):
+        self.text = text
+        self.tokenizer = bert_tokenizer
+        self.max_len = max_len
+        self.transform = nlp.data.BERTSentenceTransform(self.tokenizer, max_seq_length=self.max_len, pad=pad, pair=pair)
+        self.sentences = [self.transform([i]) for i in text]
+
+    def __len__(self):
+        return (len(self.labels))
+
+    def __getitem__(self, i):
+        return self.sentences[i]
